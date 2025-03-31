0d72ffbf72690e0a18ceebc54d2759e6c40c781c
rafroes1 <rafafroes@outlook.com>
2025-03-31 14:56:23 -0300

    Initial Commit

---
+++ HealthKikExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthKikExample
+//
+//  Created by Rafael Carvalho on 31/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthKikExample/HealthKikExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKikExampleApp.swift
+//  HealthKikExample
+//
+//  Created by Rafael Carvalho on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthKikExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
